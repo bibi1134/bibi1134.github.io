7aa680d3932c9638697aeecc2527635d74ad4a53
student <student@gwcadmins-MacBook-Pro.local>
2019-07-18 09:53:01 -0500

    intital commit

---
+++ myScript.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+
+  document.body.style.backgroundImage = "url('file:///Users/student/Desktop/bibi1134.github.io/background-texture-2110724__340.jpg')";}
+function myFunction2()
+{  document.body.style.backgroundImage = "url('download-2.jpg')"; }
+function myFunction3()
+{  document.body.style.backgroundImage = "url('images-1.jpg')"; }
+function myFunction4()
+{  document.body.style.backgroundImage = "url('file:///Users/student/Desktop/bibi1134.github.io/images.png')"; }
+function myFunction5()
+{  document.body.style.backgroundImage = "url('file:///Users/student/Desktop/bibi1134.github.io/images-3.jpg')"; }
